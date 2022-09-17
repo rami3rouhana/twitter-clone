ac293f846dfebaf445b212cfc5fb66de8872b2cb
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-17 13:35:51 +0300

    add and implement add like

---
+++ backend/add_like.php
@@ -0,0 +1,26 @@
+<?php
+
+//connection
+include("connection.php");
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+//variable
+$user_id=$_POST["user_id"];
+$post_id=$_POST["post_id"];
+
+//query
+$query=$mysqli->prepare("insert into likes (users_id,posts_id) VALUES(?,?)");
+$query->bind_param("ii", $user_id,$post_id);
+$query->execute();
+
+$result=[];
+$result["success"]=true;
+echo json_encode($result);
+
+
+
+?>
