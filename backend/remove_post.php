b02577295c2e35f4fd120f00312e446179dceb1d
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-17 14:19:28 +0300

    implement remove_post.php with connection and queries and response

---
+++ backend/remove_post.php
@@ -0,0 +1,19 @@
+<?php 
+//connection
+include("connection");
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+//variables
+$post_id=$_POST["postId"];
+//query
+$query = $mysqli->prepare("DELETE FROM posts where id='$post_id'");
+$query->execute();
+
+//response
+$response=[];
+$response["success"]=true;
+echo json_encode($response);
+
+?>
