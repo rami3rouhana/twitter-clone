b86f65457d5c7311b80ea79b18b1633101526959
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-18 12:23:52 +0300

    add user posts api

---
+++ backend/user_posts.php
@@ -0,0 +1,38 @@
+<?php
+//connection 
+include("connection.php");
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$userId = $_POST["userId"];
+
+//queries
+$query=$mysqli->prepare("
+SELECT * FROM posts 
+WHERE posts.users_id = ?
+");
+$query->bind_param("i",$userId);
+$query->execute();
+$array=$query->get_result();
+
+//response
+$response=[];
+
+//implement every row in array 
+while ($a = $array->fetch_assoc()) {
+    $response[] = $a;
+}
+//convert the into json object
+$json = json_encode($response);
+echo $json;
+
+
+
+
+
+?>
