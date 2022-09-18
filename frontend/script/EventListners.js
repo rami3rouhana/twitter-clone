3331b70182306ecccdcd7c8115409a4dda6eb397
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-18 21:25:35 +0300

    add event listeners

---
+++ frontend/script/EventListners.js
@@ -0,0 +1,26 @@
+import { addLike, addComment, removeComment, removeLike, removePost } from "./PostActions.js";
+
+export const EventListners = async ( ) => {
+
+    let postsComment = []
+
+    const commentTexts = document.getElementsByClassName("user-write-comment");
+    
+    for ( let commentText of commentTexts){
+        commentText.addEventListener("blur",(e)=>{
+            const text = e.currentTarget.value;
+            debugger
+            postsComment[e.currentTarget.parentElement.parentElement.parentElement.id]= e.currentTarget.value;
+        })    
+    }
+
+    const commentSubmits = document.getElementsByClassName("reply-btn");
+    
+    for ( let commentSubmit of commentSubmits){
+        commentSubmit.addEventListener("click",(e)=>{
+            const postId = e.currentTarget.parentElement.parentElement.parentElement.id;
+            const userId = localStorage.getItem("user_id");
+            addComment(userId,postId,postsComment[postId])
+        })    
+    }
+}
