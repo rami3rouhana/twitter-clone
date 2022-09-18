e2b941edb4554f2259281ada4063b7c456831f34
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-18 21:24:34 +0300

    refactor get comments

--- backend/get_post_comments.php
+++ backend/get_post_comments.php
@@ -11,7 +11,14 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 //variable 
 $post_id = $_POST['postId'];
 //queries
-$query = $mysqli->prepare("Select * FROM `comments`LEFT JOIN posts ON comments.posts_id = posts.id WHERE posts_id='$post_id'");
+$query = $mysqli->prepare("
+Select *,comments.id 
+FROM `comments`
+LEFT JOIN posts 
+ON comments.posts_id = posts.id 
+LEFT JOIN users 
+ON comments.users_id = users.id
+WHERE posts_id='$post_id' ");
 $query->execute();
 $array = $query->get_result();
 //result
