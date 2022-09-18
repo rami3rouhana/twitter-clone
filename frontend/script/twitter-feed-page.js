467bb2de801dc07ea2d83c2b8c920b5f401c1ce8
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-18 21:23:56 +0300

    add page restriction

--- frontend/script/twitter-feed-page.js
+++ frontend/script/twitter-feed-page.js
@@ -1,5 +1,9 @@
 import { TweetButton } from "./TweetButton.js";
 import { DisplayPosts } from "./DisplayPosts.js";
 
+debugger
+if (localStorage.getItem("user_id") === null) {
+    window.location = "../frontend/landing-page.html";
+}
 TweetButton();
 DisplayPosts();
