96b67a0529975d15a6c8336c7135278e1af41355
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-18 21:56:15 +0300

    add user user info to page

--- frontend/script/twitter-feed-page.js
+++ frontend/script/twitter-feed-page.js
@@ -1,9 +1,10 @@
 import { TweetButton } from "./TweetButton.js";
 import { DisplayPosts } from "./DisplayPosts.js";
+import { UserInfo } from "./UserInfo.js";
 
-debugger
 if (localStorage.getItem("user_id") === null) {
     window.location = "../frontend/landing-page.html";
 }
-TweetButton();
+UserInfo();
 DisplayPosts();
+TweetButton();
